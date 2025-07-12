4210115ef74a3e743d09b8ee35f9e8c7c3786627
Gabriel Lopez <gabo_71096@hotmail.com>
2025-07-12 13:37:10 -0400

    chore: update SkillsSection

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -80,6 +80,19 @@ export default function SkillsSection() {
           <motion.div variants={skillCategoryVariants}>
             <GlassCard className="p-4">
               <h3 className="text-lg font-medium mb-3 text-center md:text-left flex items-center">
+                <span className="mr-2 text-xl">🎨</span> Full Stack Development
+              </h3>
+              <div className="flex flex-wrap gap-2 justify-center md:justify-start">
+                {skills.fullStackDevelopment.map((skill, index) => (
+                  <SkillTag key={skill} skill={skill} index={index} />
+                ))}
+              </div>
+            </GlassCard>
+          </motion.div>
+
+          <motion.div variants={skillCategoryVariants}>
+            <GlassCard className="p-4">
+              <h3 className="text-lg font-medium mb-3 text-center md:text-left flex items-center">
                 <span className="mr-2 text-xl">🎨</span> Frontend Development
               </h3>
               <div className="flex flex-wrap gap-2 justify-center md:justify-start">
@@ -123,19 +136,6 @@ export default function SkillsSection() {
               </h3>
               <div className="flex flex-wrap gap-2 justify-center md:justify-start">
                 {skills.cloudAndDevOps.map((skill, index) => (
-                  <SkillTag key={skill} skill={skill} index={index} />
-                ))}
-              </div>
-            </GlassCard>
-          </motion.div>
-
-          <motion.div variants={skillCategoryVariants}>
-            <GlassCard className="p-4">
-              <h3 className="text-lg font-medium mb-3 text-center md:text-left flex items-center">
-                <span className="mr-2 text-xl">🧰</span> Tools & Services
-              </h3>
-              <div className="flex flex-wrap gap-2 justify-center md:justify-start">
-                {skills.toolsAndServices.map((skill, index) => (
                   <SkillTag key={skill} skill={skill} index={index} />
                 ))}
               </div>
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -59,60 +59,44 @@ export const education = [
 ];
 export const skills = {
   programmingLanguages: [
-    "TypeScript",
-    "JavaScript",
+    "Ruby",
     "Python",
-    "C++",
-    "Java",
-    "Solidity",
-  ],
-  frontendDevelopment: [
-    "Nextjs",
-    "Reactjs",
-    "React Native",
-    "Shadcn UI",
-    "Tailwind CSS",
+    "TypeScript",
+    "C#",
     "HTML",
     "CSS",
+    "SQL",
   ],
-  backendDevelopment: ["Nodejs", "Expressjs"],
-  databaseAndStorage: ["PostgreSQL", "Drizzle (ORM)"],
-  cloudAndDevOps: ["AWS"],
-  toolsAndServices: [
-    "Clerk (Auth)",
-    "Sanity (CMS)",
-    "Tinybird (analytics)",
-    "Zod",
-    "Sentry",
-    "Mixpanel",
-    "Trigger.dev",
+  frontendDevelopment: [
+    "React",
+    "jQuery",
+    "Bootstrap",
+    "Material UI",
+    "ChakraUI",
+    "ShadCN",
+    "TailwindCSS",
   ],
+  backendDevelopment: ["Fastify", "Express", ".NET", "FastAPI", "ElysiaJS"],
+  fullStackDevelopment: ["Ruby on Rails", "Next.js"],
+  databaseAndStorage: [
+    "PostgreSQL",
+    "MySQL",
+    "MongoDB",
+    "Prisma",
+    "ActiveRecord",
+    "Drizzle",
+    "SQLAlchemy",
+  ],
+  cloudAndDevOps: [
+    "AWS",
+    "Azure",
+    "Google Cloud Platform",
+    "Digital Ocean",
+    "Docker",
+    "Swarm",
+    "Github Actions",
+    ],
 };
-
-export const projects = [
-  {
-    title: "Net Zero Carbon Emissions",
-    github: "https://github.com/rishikesh2003/Prodigi",
-    description: [
-      "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
-      "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
-      "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
-      "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
-      "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
-    ],
-  },
-  {
-    title: "Mental Aarog",
-    github: "https://github.com/rishikesh2003/mental-aarog",
-    description: [
-      "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
-      "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
-      "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
-      "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
-      "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
-    ],
-  },
-];
 
 export const awards = [
   {
